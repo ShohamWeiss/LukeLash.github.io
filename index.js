d31eccc9ccce3b2df07eff317083226dc31e1476
LukeLash <63415678+LukeLash@users.noreply.github.com>
2020-08-31 23:43:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+
+// On Campus Button
+  document.getElementById('Button1').addEventListener('click', function() {location.href = 'index.html'}, false);
+
+// In Industry Button
+  document.getElementById('Button2').addEventListener('click', function() {location.href = 'OnCampus.html'}, false);
+
+// LinkedIn Button
+  document.getElementById("Button3").addEventListener("mouseover", onLinkedIn);
+
+  function onLinkedIn() {
+            document.getElementById('Button3').innerHTML
+                = 'Go to My LinkedIn';
+        }
+
+  document.getElementById("Button3").addEventListener("mouseout", offLinkedIn);
+
+    function offLinkedIn() {
+              document.getElementById('Button3').innerHTML
+                  = 'LinkedIn';
+          }
+
+  document.getElementById("Button3").addEventListener("click", toLinkedIn);
+
+    function toLinkedIn() {
+      window.open('https://www.linkedin.com/in/lucas-lasher-21011914a/', '_blank');
+    }
+
+// Contact Button
+  document.getElementById('Button4').addEventListener('click', function() {location.href = 'ConnectWithMe.html'}, false);
+
+
+}
